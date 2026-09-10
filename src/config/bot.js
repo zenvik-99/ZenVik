bfb97460edcc0c7b6508993f810e51b160b8b926
zenvik-99 <zenxabirgod@gmail.com>
2026-09-10 09:35:17 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name:"ZenVik ownz "Custom Status", // required by Discord API, not shown in the client
+        name:"ZenVik ownz Custom Status", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
         type: 4,               // Custom
       },
